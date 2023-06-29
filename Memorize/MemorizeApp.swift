5460635037e07befb037df75cc1a7875410005f3
Duy Nguyen <nkduy2010@gmail.com>
2023-06-28 23:42:23 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Duy Nguyen on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
